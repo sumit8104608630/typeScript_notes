53b0f0de0aca04d94d501dd05a5ce67f919c61c1
sumit sharma <sumit8104608630@gmail.com>
2024-06-24 19:18:36 +0530

    first commit

---
+++ array.js
@@ -0,0 +1,19 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// let's dive into  array in the typescript
+// array is a collection of similar data types
+// let's create an array of numbers
+var array = [];
+// if now you can see it will show all the array method and properties
+// let's add some elements to the array
+array.push(10, 5, 6, 8, 3, 2, 1);
+// now let's see the array
+console.log(array);
+// let's se second way 
+var array2 = [1, 2, 3, 4, 5];
+// let's see the array
+console.log(array2);
+// let's try nested  array
+var nestedArray = [];
+nestedArray.push([1, 2, 3], [4, 5, 6], [7, 8, 9]);
+console.log(nestedArray);
